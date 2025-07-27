d427c48a47352e3c07afa3b7a775550f61409ae0
sukanyakake <kdmrsukanya@gmail.com>
2025-07-27 12:12:19 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (13.21%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -6,12 +6,7 @@ public:
         int size=columnTitle.size();
         while(i<size){
             int val=(columnTitle[i]-'A')+1;
-            cout<<"index "<<val<<" pow ";
-            int power=pow(26,(size-i-1));
-            cout<<power<<" ";
-            cout<<"  prev res "<<res;
             res=res+(pow(26,(size-i-1))*val);
-            cout<<" "<<res<<endl;
             i++;
         }
         return res;
