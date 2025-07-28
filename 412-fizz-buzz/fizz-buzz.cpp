54d04eb81525b2d26dd0eb5a88f1a40f56741ddc
sukanyakake <kdmrsukanya@gmail.com>
2025-07-29 00:58:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (16.18%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
+    string fizbuz(int n){
+        if(n%3==0 and n%5==0){
+            return "FizzBuzz";
+        }else if(n%3==0){
+            return "Fizz";
+        }else if(n%5==0){
+            return "Buzz";
+        }else{
+            return to_string(n);
+        }
+    }
     vector<string> fizzBuzz(int m) {
         vector<string> res;
         for (int n=1;n<=m;n++){
-            if(n%3==0 and n%5==0){
-            res.push_back("FizzBuzz");
-        }else if(n%3==0){
-            res.push_back("Fizz");
-        }else if(n%5==0){
-            res.push_back("Buzz");
-        }else{
-            res.push_back(to_string(n));
-        }
+           res.push_back(fizbuz(n));
         }
         return res;
     }
