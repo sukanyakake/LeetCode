3f3df61e4e1cf9b1209dd0effb99d915df3bf875
sukanyakake <kdmrsukanya@gmail.com>
2025-07-29 15:43:51 +0530

    Time: 2 ms (48.17%) | Memory: 10.5 MB (44.06%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseVowels(string str) {
+        string s="";
+
+        for(char ch:str){
+            if(ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U' or ch=='a' or ch=='e' or ch=='i' or  ch=='o'  or  ch=='u'){
+                s+=ch;
+            }
+        }
+        reverse(s.begin(),s.end());
+        int j=0;
+        for(int i=0;i<str.size();i++){
+            char ch=str[i];
+            if(ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U' or ch=='a' or ch=='e' or ch=='i' or  ch=='o'  or  ch=='u'){
+                str[i]=s[j];
+                j++;
+            }
+        }
+        return str;
+    }
+};
