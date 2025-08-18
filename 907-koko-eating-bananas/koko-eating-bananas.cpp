9716e051f30b431e1d1d196766f49a613d4fd877
sukanyakake <kdmrsukanya@gmail.com>
2025-08-18 12:36:19 +0530

    Time: 14 ms (23.89%) | Memory: 39.3 MB (5.63%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -11,16 +11,14 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         long long high=*max_element(piles.begin(),piles.end());
         long long low=1;
-        int ans=0;
         while(low<=high){
             long long mid=low+(high-low)/2;
             if(okay(mid,h,piles)){
-                ans=mid;
                 high=mid-1;
             }else{
                 low=mid+1;
             }
             }
-        return ans;
+        return low;
     }
 };
