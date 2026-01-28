7fe139252f7ec91efe111f437f1ec19f9d914d64
sukanyakake <kdmrsukanya@gmail.com>
2026-01-28 20:23:45 +0530

    Time: 634 ms (8.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select activity_date as day,count(distinct user_id) as active_users from Activity 
+ where (activity_type in ('open_session', 'end_session', 'scroll_down', 'send_message')) and (activity_date between '2019-06-28' and '2019-07-28')
+group by activity_date order by activity_date;
