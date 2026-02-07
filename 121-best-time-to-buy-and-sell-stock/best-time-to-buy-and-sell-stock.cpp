2a7fe09b2591e7fc30348583143a3349e5d54cb3
sukanyakake <kdmrsukanya@gmail.com>
2026-02-07 09:20:30 +0530

    Time: 4 ms (18.20%) | Memory: 97.4 MB (60.02%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -3,13 +3,22 @@ public:
     int maxProfit(vector<int>& prices) {
         if(prices.size()==0) return 0;
         int mp=0;
-        int bestBuy=prices[0];
+        int bestBuy=0;
+        int sellDay=0;
         for(int i=1;i<prices.size();i++){
-            if(bestBuy<prices[i]){
-                mp=max(mp,prices[i]-bestBuy);
+            if(prices[bestBuy]<prices[i]){
+                int temp=prices[i]-prices[bestBuy];
+                if(mp<temp){
+                    mp=temp;
+                    sellDay=i;
+                }
             }
-            bestBuy=min(bestBuy,prices[i]);
+            if(prices[bestBuy]>prices[i]){
+                bestBuy=i;
+            }
         }
+
+        cout<<sellDay<<" "<<bestBuy;
         return mp;
     }
 };
